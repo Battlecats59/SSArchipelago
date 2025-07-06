de5d37daee6fa4e00dfe1071c8bb5b5adf3b31a6
Battlecats59 <awelch6206@gmail.com>
2025-07-05 22:32:37 -0400

    Fix error with 0.6.2 containers

--- worlds/ss/__init__.py
+++ worlds/ss/__init__.py
@@ -12,7 +12,7 @@ import yaml
 from BaseClasses import MultiWorld, Region, Tutorial, LocationProgressType
 from Options import Toggle, OptionError
 from worlds.AutoWorld import WebWorld, World
-from worlds.Files import APContainer, AutoPatchRegister
+from worlds.Files import APPlayerContainer, AutoPatchRegister
 from worlds.generic.Rules import add_item_rule
 from worlds.LauncherComponents import (
     Component,
@@ -84,7 +84,7 @@ class SSWeb(WebWorld):
     rich_text_options_doc = True
 
 
-class SSContainer(APContainer, metaclass=AutoPatchRegister):
+class SSContainer(APPlayerContainer, metaclass=AutoPatchRegister):
     """
     This class defines the container file for Skyward Sword.
     """
